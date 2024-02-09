6db6d7447f5bfebee263d2ad18841d9efca38f2f
tsokac2 <tsokac2@gmail.com>
2024-02-09 17:12:49 +0000

    Updating

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,75 @@
+
+// Project Type enums
+
+enum ProjectStatus {Active, Finished}
+
+// #4 - CLASS for individual Project
+class Project {
+    constructor(
+        public id: string, 
+        public title:string, 
+        public description: string, 
+        public people: number, 
+        public status: ProjectStatus )
+        {}
+}
+
+
+
+
+
+// #3 - CLASS Project State Managment Global Class for managing State 
+
+type Listener = (items: Project[]) => void;
+
+class ProjectState {
+    private listeners: Listener[] = [];
+    private projects: Project[] = [];
+    private static instance: ProjectState;
+   
+
+    private constructor() {
+
+    }
+
+    addListener(listenerFn: Listener) {
+        this.listeners.push(listenerFn);
+    }
+
+    static getInstance() {
+        if(this.instance) {
+            return this.instance;
+        }
+
+        this.instance = new ProjectState();
+        return this.instance
+    }
+
+    addProject(title: string, description:string, numOfPeople: number) {
+
+        const newProject = new Project (
+            Math.random().toString(),
+            title,
+            description,
+            numOfPeople,
+            ProjectStatus.Active
+        );
+
+        this.projects.push(newProject);
+
+        for(const listenerFn of this.listeners) {
+            listenerFn(this.projects.slice());
+        }
+
+    }
+}
+
+// Global constant that can talk to any class in the application
+
+
+const projectState = ProjectState.getInstance();
+
+
 // Validation logic that can me implemented acros the project in different parts of the app
 interface Validatable {
     value: string | number;
@@ -55,123 +127,225 @@ function autobind(
 
 
 
-// Main ProjectInput Class - Tomislav
+// #1 - CLASS Main ProjectInput Class for rendering Main Template
+// CLASS - Master class that will store all the data that is repeted acrose another classes
+// Component Base Class
 
-class ProjectInput { //showing form in the Html Templat element
-
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     templateElement: HTMLTemplateElement; // Set type of templateElement
-    hostElement: HTMLDivElement; // Set type of hostElement
-    element: HTMLFormElement; // Set type of element
-
-    titleInputElement: HTMLInputElement;
-    descriptionInputElement: HTMLInputElement;
-    peopleInputElement: HTMLInputElement;
-
-
-    // Class constructor
-    constructor(){
-        this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement; //what you fetch here will not be null and it will be of that type
-        this.hostElement = document.getElementById("app")! as HTMLDivElement;
-
-        const importedNode = document.importNode(this.templateElement.content, true);
-
-        this.element = importedNode.firstElementChild as HTMLFormElement;
-        this.element.id = 'user-input'; //adding id with css styling
-
-        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
-        this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
-        this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
-
-        this.configure();
-        this.attach();
+    hostElement: T; // Set type of hostElement
+    element: U; // Set type of element
+  
+    constructor(
+      templateId: string,
+      hostElementId: string,
+      insertAtStart: boolean,
+      newElementId?: string
+    ) {
+      this.templateElement = document.getElementById(
+        templateId
+      )! as HTMLTemplateElement;
+      this.hostElement = document.getElementById(hostElementId)! as T;
+  
+      const importedNode = document.importNode(
+        this.templateElement.content,
+        true
+      );
+      this.element = importedNode.firstElementChild as U;
+  
+      if (newElementId) {
+        this.element.id = newElementId;
+      }
+  
+      this.attach(insertAtStart);
     }
+  
+    private attach(insertAtBeginning: boolean) {
+      this.hostElement.insertAdjacentElement(
+        insertAtBeginning ? "afterbegin" : "beforeend",
+        this.element
+      );
+    }
+  
+    abstract configure(): void;
+    abstract renderContent(): void;
+  
+  }
 
-    // Collect users input from the front-end
+// ProjectInput Class
 
-    private gatherUserInput(): [string, string, number] | void {//type tuple or void (reeplacment for a undefind)
-        const enteredTitle = this.titleInputElement.value;
-        const enteredDescription = this.descriptionInputElement.value;
-        const enteredPeople = this.peopleInputElement.value;
+class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+  //showing form in the Html Templat element
 
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
-        // Constructing validable objects base on the validate function and validable interface
+  // Class constructor
+  constructor() {
+    super("project-input", "app", true, "user-input");
 
-        //Object for validating the title
-        const titleValidatable: Validatable = {
-            value: enteredTitle,
-            required: true,
-            minLength: 3
-        }; 
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    )! as HTMLInputElement;
 
-        //Object for validating description
-        const descriptionValidatable: Validatable = {
-            value: enteredDescription,
-            required: true,
-            minLength: 5
-        }; 
-
-        //Object for validating people
-        const peopleValidatable: Validatable = {
-            value: +enteredPeople,
-            required: true,
-            min: 1,
-            max: 8
-        }; 
+    this.configure();
+  }
 
 
-        // Final validation
-        if (
-          !validate(titleValidatable) || 
-          !validate(descriptionValidatable) || 
-          !validate(peopleValidatable)
-        ) {
-            alert("Invalid input, please try again!");
-            return;
-        } else {
-            
-            return [enteredTitle, enteredDescription, +enteredPeople]; // +enteredPeople converting to a number
+  configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
+  renderContent() {}
+
+  // Collect users input from the front-end
+
+  private gatherUserInput(): [string, string, number] | void {
+    //type tuple or void (reeplacment for a undefind)
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    // Constructing validable objects base on the validate function and validable interface
+
+    //Object for validating the title
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+      minLength: 3,
+    };
+
+    //Object for validating description
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+
+    //Object for validating people
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 8,
+    };
+
+    // Final validation
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert("Invalid input, please try again!");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople]; // +enteredPeople converting to a number
+    }
+  }
+
+  // after button is submited clear the form
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
+  // using the decorator when submiting the form
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault(); //preventing default subbision by the browser
+    // console.log(this.titleInputElement.value); // logs in the console content of the id="title" element
+    const userInput = this.gatherUserInput();
+
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+
+      projectState.addProject(title, desc, people);
+      this.clearInputs();
+    }
+  }
+
+  // Connecting submitHandelr method to the button in the HTML - with the type 'submit' -  using the bind method to avoid an errors
+ 
+
+  // Move to base Component class - Attaching template element to the the host element that is rendering template form element to the DOM -
+  // private attach() {
+  //     this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  // }
+}
+//#---------------------------------------------------------#
+
+
+
+
+//#---------------------------------------------------------#
+// #2 - CLASS Rendering Project List Calss
+// ProjectList Class 
+
+class ProjectList extends Component<HTMLDivElement, HTMLElement> {
+  assignedProjects: Project[];
+
+  constructor(private type: "active" | "finished") {
+    // assing a dynamic calss for different part of the imported elements
+    super("project-list", "app", false, `${type}-projects`);
+    this.assignedProjects = [];
+
+    projectState.addListener((projects: Project[]) => {
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.Active;
         }
+        return prj.status === ProjectStatus.Finished;
+      });
+
+      this.assignedProjects = relevantProjects;
+      this.renderProjects();
+    });
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure(){}
+
+  renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId; // creating an ID for the 'ul' element  - ! means that ul will not be null
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+
+  renderProjects() {
+    const listEl = document.getElementById(
+      `${this.type}-projects-list`
+    )! as HTMLUListElement;
+
+    listEl.innerHTML = "";
+
+    for (const prjItem of this.assignedProjects) {
+      const listItem = document.createElement("li");
+      listItem.textContent = prjItem.title;
+      listEl.appendChild(listItem);
     }
-
-    // after button is submited clear the form
-    private clearInputs() {
-        this.titleInputElement.value = '';
-        this.descriptionInputElement.value = '';
-        this.peopleInputElement.value = '';
-    }
-
-
-    // using the decorator when submiting the form 
-    @autobind
-
-    private submitHandler(event: Event) {
-        event.preventDefault(); //preventing default subbision by the browser
-        // console.log(this.titleInputElement.value); // logs in the console content of the id="title" element
-      const userInput = this.gatherUserInput();
-
-        if(Array.isArray(userInput)){
-            const [title, desc, people] = userInput;
-            console.log(title, desc, people);
-            this.clearInputs();
-        }
-    }
-    
-
-    // Connecting submitHandelr method to the button in the HTML - with the type 'submit' -  using the bind method to avoid an errors
-    private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
-    }
-
-
-    // Attaching template element to the the host element that is rendering template form element to the DOM
-    private attach() {
-        this.hostElement.insertAdjacentElement('afterbegin', this.element);
-    }
+  }
 }
 
 
+// #-------------------------------------------------# 
+
 // Creating and calling a new calss and rendering elemetn to the DOM
 const prInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
 
 
 // Cheking the compiler conneciton 
